a1304d2927af9b1d198583d1b4a86dc9b14dba07
csipkeskombine@gmail.com <csipkeskombine@gmail.com@19cb145c-d5b9-dcde-f43f-4d4427ca961f>
2010-09-15 00:57:19 +0000

    animation finished, 1.1

--- trunk/eng/src/com/nonoo/dukenukem/widget/DukeNukemWidgetProvider.java
+++ trunk/eng/src/com/nonoo/dukenukem/widget/DukeNukemWidgetProvider.java
@@ -5,29 +5,40 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 public class DukeNukemWidgetProvider extends AppWidgetProvider {
 
+	@Override
+	public void onDisabled(Context context) {
+		android.os.Process.killProcess(android.os.Process.myPid());
+		super.onDisabled(context);
+	}
+	
+	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int N = appWidgetIds.length;
-
         // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
+        for (int i=0; i < appWidgetIds.length; i++) {
             int appWidgetId = appWidgetIds[i];
 
             // Create an Intent to launch activity
             Intent intent = new Intent(context, DukeNukemWidgetService.class);
-            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+            intent.putExtra( AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId );
+            Uri data = Uri.withAppendedPath( Uri.parse("countdownwidget://widget/id/#update" + appWidgetId), String.valueOf( appWidgetId ) );
+            intent.setData(data);
+            intent.setAction( "update" );
+            PendingIntent pendingIntent = PendingIntent.getService( context, 0, intent, 0 );
 
             // Get the layout for the App Widget and attach an on-click listener to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
-            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+            views.setOnClickPendingIntent( R.id.button, pendingIntent );
 
             // Tell the AppWidgetManager to perform an update on the current App Widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
-    }
+
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
 
 }
