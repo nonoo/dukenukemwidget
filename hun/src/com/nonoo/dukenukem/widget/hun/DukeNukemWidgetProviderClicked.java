bc2c50e807028026c49f03ea53d6077a940678a5
csipkeskombine@gmail.com <csipkeskombine@gmail.com@19cb145c-d5b9-dcde-f43f-4d4427ca961f>
2011-04-13 16:44:46 +0000

    stuck widget fix

--- hun/src/com/nonoo/dukenukem/widget/hun/DukeNukemWidgetProviderClicked.java
+++ hun/src/com/nonoo/dukenukem/widget/hun/DukeNukemWidgetProviderClicked.java
@@ -8,6 +8,8 @@ public class DukeNukemWidgetProviderClicked extends DukeNukemWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
 		for (int appWidgetId : appWidgetIds) {
 			Intent i = new Intent(context, DukeNukemWidgetService.class);
 			i.putExtra("appWidgetId", appWidgetId);
